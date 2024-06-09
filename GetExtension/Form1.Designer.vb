803b886ef579b34e3e24c0d2d3d6a5d6f11a0c4a
ChuCong <2conglc.vn@gmail.com>
2024-06-09 14:49:57 +0700

    update

---
+++ GetExtension/Form1.Designer.vb
@@ -0,0 +1,82 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        OpenFileDialog1 = New OpenFileDialog()
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(161, 121)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(156, 23)
+        Button1.TabIndex = 0
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' OpenFileDialog1
+        ' 
+        OpenFileDialog1.Filter = "All Files (*.*)|*.*"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(161, 39)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(169, 23)
+        TextBox1.TabIndex = 1
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(67, 42)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(88, 15)
+        Label1.TabIndex = 2
+        Label1.Text = "File Extension : "
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(456, 266)
+        Controls.Add(Label1)
+        Controls.Add(TextBox1)
+        Controls.Add(Button1)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        MaximizeBox = False
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+
+End Class
---
+++ GetExtension/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+
+    <Obsolete>
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+
+                Dim temp As String = GetExtension(IO.File.ReadAllBytes(OpenFileDialog1.FileName))
+                TextBox1.Text = temp
+
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
